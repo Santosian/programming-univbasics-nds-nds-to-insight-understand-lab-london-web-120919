dd97a4a3a217c11963b7da29381adf535c791ae3
Santiago Molinari <santomolinari@gmail.com>
2019-11-25 23:52:11 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,9 +5,6 @@ require 'directors_database'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   pp directors_database
-  
-  
-  nil
 end
 
 def print_first_directors_movie_titles
