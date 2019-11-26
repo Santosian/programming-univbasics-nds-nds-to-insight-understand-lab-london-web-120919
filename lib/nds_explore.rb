7b18255f43f42df71b8273a9dd4ee5bae13fb954
Santiago Molinari <santomolinari@gmail.com>
2019-11-26 00:43:08 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -21,6 +21,7 @@ def print_first_directors_movie_titles
   puts dd[0][:movies][4][:title] 
   puts dd[0][:movies][5][:title]
   
+  
   while idx < ??? do
     puts dd[0][:movies][idx][:title]
     idx += 1 
